b49b8a7925e115b2630c5dd79a049dfcf1ee0655
alexkapralov <bopr2001@mail.ru>
2022-10-17 00:18:10 +0300

    task 2.2.1 added

--- main.c
+++ main.c
@@ -4,35 +4,73 @@
 void types_length();
 void symbols_pos();
 void root(double eps, int x);
+float power(float x, int i);
+void gornerOne();
 
 int main() {
 
-    //task 1, exercise 1
-    //prints length of: short, int, long, float, double, long double
-    printf("Task 1.1\n");
-    types_length();
-    printf("\n");
+//    //task 1.1
+//    //prints length of: short, int, long, float, double, long double
+//    printf("Task 1.1\n");
+//    types_length();
+//    printf("\n");
+//
+//    //task 1.4
+//    printf("Task 1.2\n");
+//    symbols_pos();
+//    printf("\n");
+//
+//    //task 2.1
+//    int x = 0;
+//    double eps;
+//    printf("Enter epsilon: \n");
+//    scanf("%lf", &eps);
+//    while (x != -1) {
+//        printf("Enter the number: \n");
+//        scanf("%d", &x);
+//        root(eps, x);
+//    }
 
-    //task 1, exercise 4
-    printf("Task 1.2\n");
-    symbols_pos();
-    printf("\n");
-
-    //task 2.1
-    int x = 0;
-    double eps;
-    printf("Enter epsilon: \n");
-    scanf("%lf", &eps);
-    while (x != -1) {
-        printf("Enter the number: \n");
-        scanf("%d", &x);
-        root(eps, x);
-    }
+    //task 2.2.1
+    gornerOne();
 
     return 0;
 }
 
 
+//task 2.2.1
+void gornerOne(){
+    int n;  //number of numbers
+    float x;  //const
+    float a;  //a(i) coefficient
+    float p = 0;  //func P(x)
+
+    printf("enter x: ");  //enter once
+    scanf("%f", &x);  //enter once
+    printf("enter n: ");  //enter once
+    scanf("%d", &n);  //enter once
+
+    for (int j = 0; j < n; ++j) {
+        printf("enter a%d: ", j);
+        scanf("%f", &a);  //n times
+
+        p = p + a * power(x,j);
+    }
+    printf("P(x): %f\n", p);
+}
+
+//support power func
+float power(float x, int i){
+    float p = 1;
+    if (i > 0) {
+        for (int j = 0; j < i; ++j) {
+            p = p * x;
+        }
+    }
+    return p;
+}
+
+//task 1.1
 void symbols_pos(){
 
     printf("Positions of symbols '0'-'9', 'a'-'z', 'A'- 'Z' are:\n");
@@ -61,6 +99,7 @@ void symbols_pos(){
 
 }
 
+//task 1.4
 void types_length() {
     int i;
 
@@ -83,6 +122,7 @@ void types_length() {
     printf("Size of long double: %d\n", i);
 }
 
+// task 2.1
 void root(double eps, int x){
     double xi, root;
     eps = 0.0000001;
