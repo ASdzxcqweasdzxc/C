22974798efe8b2413f9737b242af6f0977709e9b
alexkapralov <bopr2001@mail.ru>
2022-12-10 18:09:16 +0300

    corrected tasks 2.2.2 and 2.2.3: works properly now

--- main.c
+++ main.c
@@ -34,37 +34,37 @@ int main() {
 
     //task 2.3
     //prints fibonacci sequence
-    int i, c;
-    while(1) {
-        int f_prev=0, f_cur=1, tmp;
-        printf("Enter value i: ");
-        c = scanf("%d", &i);
-        if (c == EOF) break;
-        printf("Iterative value= %d\n", f_iter(i));
-        printf("Recursive value= %d\n", fibonacci(i));
-    }
+//    int i, c;
+//    while(1) {
+//        int f_prev=0, f_cur=1, tmp;
+//        printf("Enter value i: ");
+//        c = scanf("%d", &i);
+//        if (c == EOF) break;
+//        printf("Iterative value= %d\n", f_iter(i));
+//        printf("Recursive value= %d\n", fibonacci(i));
+//    }
 
 
     // task 2.2.2
     // gornerOne();
 
-//    double x, x_n = 1, a, p_x = 0, p_dev = 0;
-//    int n = 0, c;
-//    printf("Enter value x:\n");
-//    scanf("%lf", &x);
-//    while (1) {
-//        printf("Enter value a%d:\n", n++);
-//        c = scanf("%lf", &a);
-//        if (c == EOF)
-//            break;
-////        printf("x_%d=%.3lf\n", n++, x_n);
-//        p_x += a * x_n;
-//        p_dev += (a * (x_n / x)) * (n - 1);
-////        printf("px=%.3lf\n", p_dev);
+    double x, x_n = 1, a, p_x = 0, p_dev = 0;
+    int n = 0, c;
+    printf("Enter value x:\n");
+    scanf("%lf", &x);
+    while (1) {
+        printf("Enter value a%d:\n", n++);
+        c = scanf("%lf", &a);
+        if (c == EOF)
+            break;
+//        printf("x_%d=%.3lf\n", n++, x_n);
+        p_dev = p_dev * x + p_x;
+        p_x = p_x * x + a;
+//        printf("px=%.3lf\n", p_dev);
 //        x_n *= x;
-//    }
-//    printf("P(%.3lf)=%.3lf\n", x, p_x);
-//    printf("P_dev=%.3lf\n", p_dev);
+    }
+    printf("P(%.3lf)=%.3lf\n", x, p_x);
+    printf("P_dev=%.3lf\n", p_dev);
     return 0;
 }
 
